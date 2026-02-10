9073142db0493c07d997159951585522010128ad
github-action-benchmark <github@users.noreply.github.com>
2026-02-10 06:12:14 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7d8f8ddf5e7cf335108acd5282a783e835371e0d

---
+++ dev/test-duration/macos-latest/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770703934665,
+  "repoUrl": "https://github.com/Mkerian10/pluto",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "33331268+Mkerian10@users.noreply.github.com",
+            "name": "Matthew Kerian",
+            "username": "Mkerian10"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7d8f8ddf5e7cf335108acd5282a783e835371e0d",
+          "message": "Merge pull request #12 from Mkerian10/fix-ci\n\nFix CI: rust_ffi linker failures + flaky channel timeouts + test duration tracking",
+          "timestamp": "2026-02-10T00:10:52-06:00",
+          "tree_id": "4bb3d5bfe7ce2822ae7608c06bab44a662bcd64d",
+          "url": "https://github.com/Mkerian10/pluto/commit/7d8f8ddf5e7cf335108acd5282a783e835371e0d"
+        },
+        "date": 1770703934275,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "cargo_test",
+            "value": 61796,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
